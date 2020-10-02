2a8e86cb114dc7c02ce34abdc384f461e10ad43a
mmtakeuchi <mikemtaki@gmail.com>
2020-10-02 13:50:11 -0700

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import uuid from "uuid";
  
 const rootReducer = combineReducers({
   authors: authorsReducer,
@@ -31,16 +32,6 @@ function authorsReducer(state = [], action) {
     case "REMOVE_AUTHOR":
       idx = state.findIndex(author => author.id  === action.id)
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
-
-    case "ADD_BOOK":
-      let existingAuthor = state.filter(
-        author => author.authorName === action.book.authorName
-      );
-      if (existingAuthor.length > 0) {
-        return state;
-      } else {
-        return [...state, { authorName: action.book.authorName, id: uuid() }];
-      }
  
     default:
       return state;
