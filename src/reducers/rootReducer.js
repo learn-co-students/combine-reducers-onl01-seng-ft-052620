95f8c6880fcbae5880f2a6e4a7899ec47ee4d20e
SWFullPotential <swfullpotential@gmail.com>
2020-10-06 12:01:15 -0600

    Done.

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+import authorsReducer from 'reducers/authorsReducer';
+import booksReducer from 'reducers/booksReducer';
+
+const rootReducer = combineReducers({
+    authors: authorsReducer, 
+    books: booksReducer
+});
+
+export default rootReducer;
+
